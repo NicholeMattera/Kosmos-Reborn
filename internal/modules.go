2f984043d722473eaef85e4e6684e9eea13e4cfd
Nichole Mattera <me@nicholemattera.gay>
2023-09-12 17:19:51 -0400

    🗑️ Removed Gitea support

--- internal/modules.go
+++ internal/modules.go
@@ -1,16 +1,16 @@
 // Kosmos Reborn Builder
 // Copyright (C) 2022 Nichole Mattera
-// 
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -103,10 +103,6 @@ func BuildModules(tempDirectory string, version string, githubUsername string, g
 }
 
 func GetLatestRelease(source string, organization string, repository string, assetPattern string, githubUsername string, githubPassword string) (string, string, string, error) {
-	if source == "NicholeMattera" {
-		return GetLatestGiteaRelease("git.nicholemattera.com", organization, repository, assetPattern)
-	}
-
 	return GetLatestGitHubRelease(organization, repository, assetPattern, githubUsername, githubPassword)
 }
 
